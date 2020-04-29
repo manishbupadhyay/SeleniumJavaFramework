31b8810f1f18eb719b7ae79c28a72553c0dce6b9
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-04-29 20:20:23 +0530

    Add chrome driver and run test on chrome browser.
    Add chrome driver and run test on chrome browser.
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

--- src/test/java/BrowserTest.java
+++ src/test/java/BrowserTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserTest {
@@ -8,10 +9,16 @@ public class BrowserTest {
 		String projectPath = System.getProperty("user.dir");
 		System.out.println("projectPath :" + projectPath);	
 		
-		// set gecko driver path
-		System.setProperty("webdriver.gecko.driver", projectPath + "//drivers/geckodriver/geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://selenium.dev");
+		// set gecko driver path for firefox browser
+		System.setProperty("webdriver.gecko.driver", projectPath + "/drivers/geckodriver/geckodriver.exe");
+		WebDriver firefoxDriver = new FirefoxDriver();
+		firefoxDriver.get("http://selenium.dev");
+		
+		// set chrome drive to run test on chrome browser
+		System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver/chromedriver.exe");
+		WebDriver chromeDriver = new ChromeDriver();
+		chromeDriver.get("http://selenium.dev");
+		chromeDriver.close();
 	}
 
 }
