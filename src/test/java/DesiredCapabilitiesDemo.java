15277895a6fc49cd2540b5425e4f4d2668062462
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-05-09 02:01:45 +0530

    Desired Capabilities in Selenium Java and some missing files.

---
+++ src/test/java/DesiredCapabilitiesDemo.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DesiredCapabilitiesDemo {
+
+	
+	public static void main(String[] args) {
+		
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability("ignoreProtectedModeSettings", true);
+		
+		WebDriverManager.iedriver().setup();
+		WebDriver driver = new InternetExplorerDriver(dc);
+		
+		driver.get("http://google.com");
+		driver.findElement(By.name("q")).sendKeys("SOftware Development Engineer Test");
+		driver.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
+		
+		driver.close();
+		driver.quit();
+	}
+
+}
