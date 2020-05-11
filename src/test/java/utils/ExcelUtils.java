341f34b1afc6096868fcbf3314b4743b9e04c474
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-05-12 00:33:29 +0530

    Add poi-ooxml maven dependency in pom.xml. Add excel libraries.Create excel read function getRowCount and getCellData and call excel function.
    Add poi-ooxml maven dependency in pom.xml. Add excel libraries.Create excel read function getRowCount and getCellData and call excel function.
    
    Co-Authored-By: Manish Upadhyay <ermanishupadhyay@gmail.com>

---
+++ src/test/java/utils/ExcelUtils.java
@@ -0,0 +1,64 @@
+package utils;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtils {
+	
+	static String projectPath;
+	static XSSFWorkbook workbook;
+	static XSSFSheet sheet;
+	
+	// constructor
+	public ExcelUtils(String excelPath, String sheetName) {
+		try {
+		    workbook = new XSSFWorkbook(excelPath);
+			sheet = workbook.getSheet(sheetName);
+		}catch(Exception exp) {
+			exp.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		getRowCount();
+		getCellDataString(0,0);
+		getCellDataNumber(1,1);
+	}
+
+	public static void getRowCount() {
+		try {
+			int rowCount = sheet.getPhysicalNumberOfRows();
+			System.out.println("No of rows: " + rowCount);
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static void getCellDataString(int rowNum, int colNum) {
+		
+		try {
+			String cellData = sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
+			System.out.println("Cell data : " + cellData);
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+			e.printStackTrace();
+		}
+	}
+	
+public static void getCellDataNumber(int rowNum,int colNum) {
+		
+		try {
+			double cellDataNumber = sheet.getRow(rowNum).getCell(colNum).getNumericCellValue();
+			System.out.println("Cell data Number : " + cellDataNumber);
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getCause());
+			e.printStackTrace();
+		}
+	}
+
+}
